d54b8140047156fb1ac8409d3a269b4c3842c20b
kuqmua <serezhashmakov@outlook.com>
2022-06-06 19:16:31 +0300

    #[tonic::async_trait]

--- src/server.rs
+++ src/server.rs
@@ -6,6 +6,11 @@ use payments::{BtcPaymentResponse, BtcPaymentRequest};
 pub mod payments {
     tonic::include_proto!("payments");
 }
+#[derive(Debug, Default)]
+pub struct BitcoinService {}
+
+#[tonic::async_trait]
+impl Bitcoin for BitcoinService{}
 
 fn main() {
     println!("Hello, world!");
