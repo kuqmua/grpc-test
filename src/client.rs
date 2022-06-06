9b22aabb971956f063346b24cd5d7c8d0d37b245
kuqmua <serezhashmakov@outlook.com>
2022-06-06 19:44:58 +0300

    impl client

--- src/client.rs
+++ src/client.rs
@@ -5,4 +5,15 @@ pub mod payments {
     tonic::include_proto!("payments");
 }
 
-fn main() {}
+#[tokio::main]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client =  BitcoinClient::connect("http://[::1]:50051").await?;
+    let request = tonic::Request::new(
+        BtcPaymentRequest {
+            from_addr: "123456".to_owned(),
+            to_addr: "654321".to_owned(),
+            amount: 22,
+        }
+    );
+    Ok(())
+}
