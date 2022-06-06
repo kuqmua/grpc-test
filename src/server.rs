fadab8ef8f7b99467697b043cf3d250cd5241faf
kuqmua <serezhashmakov@outlook.com>
2022-06-06 19:27:41 +0300

    async fn send_payment

--- src/server.rs
+++ src/server.rs
@@ -10,7 +10,22 @@ pub mod payments {
 pub struct BitcoinService {}
 
 #[tonic::async_trait]
-impl Bitcoin for BitcoinService{}
+impl Bitcoin for BitcoinService{
+    async fn send_payment(
+        &self, 
+        request: Request<BtcPaymentRequest>,
+    ) -> Result<Response<BtcPaymentResponse>, Status>{
+        println!("Got a request: {:?}", request);
+
+        let req = request.into_inner();
+
+        let reply = BtcPaymentResponse {
+            successful: true,
+            message: format!("Sent {}BTC to {}.", req.amount, req.to_addr).into(),
+        };
+        Ok(Response::new(reply))
+    }
+}
 
 fn main() {
     println!("Hello, world!");
