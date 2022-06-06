c50b836b9718991fbb1f043a71fbd919c46c5b2e
kuqmua <serezhashmakov@outlook.com>
2022-06-06 19:14:50 +0300

    pub mod payments

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,12 @@
+use tonic::{transport::Server, Request, Response, Status};
+
+use payments::Bitcoin_server::{Bitcoin, BitcoinServer};
+use payments::{BtcPaymentResponse, BtcPaymentRequest};
+
+pub mod payments {
+    tonic::include_proto!("payments");
+}
+
 fn main() {
     println!("Hello, world!");
 }
